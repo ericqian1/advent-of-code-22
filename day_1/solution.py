8628b9ab7cbb958d1836cc3e8cc43cef7516a9c9
Qian <tianze.qian@pxd.com>
2022-12-01 09:25:17 -0600

    first day

---
+++ day_1/solution.py
@@ -0,0 +1,37 @@
+## Part 1
+with open("data.txt") as f:
+    max_total = 0
+    current_total = 0
+    for line in f.readlines():
+        try:
+            current_total += int(line)
+        except:
+            max_total = current_total if max_total < current_total else max_total
+            current_total = 0
+
+print(current_total)
+
+## Part 2 
+top_three = [0,0,0]
+with open("data.txt") as f:
+    max_total = 0
+    current_total = 0
+    new_idx = -1
+    for line in f.readlines():
+        try:
+            current_total += int(line)
+        except:
+            for idx in range(len(top_three)):
+                if current_total > top_three[idx]:
+                    new_idx = idx
+            # Avoid sorting functions by using a sorted array
+            if new_idx==-1:
+                pass
+            else:
+                top_three.insert(new_idx+1, current_total)
+                del top_three[0]
+            current_total = 0
+            new_idx = -1
+
+print(sum(top_three))
+
